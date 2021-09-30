5c9b9e1d3c1ba9cced07319b9451a799e49684b3
bbaudry <bruno.baudry@salt.ch>
2021-09-30 17:57:47 +0200

    correcting images

---
+++ source/coverage/gis/CheckIfCloseToBlackCell.hx
@@ -0,0 +1,18 @@
+package coverage.gis;
+
+import tstool.process.Descision;
+
+/**
+ * ...
+ * @author bb
+ */
+class CheckIfCloseToBlackCell extends Descision 
+{
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+}
